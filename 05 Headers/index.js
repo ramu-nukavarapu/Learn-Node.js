5fb30c3d31182eb0f938c2e4dec308f1c4a5716d
ramu-nukavarapu <ramunukavarapu2004@gmail.com>
2025-02-07 11:51:25 +0530

    Learn 'Headers' Concept

---
+++ 05 Headers/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get("/",(req,res)=>{
+    res.send("Hi from the server")
+})
+
+// Headers are parameters that comes from the URL Header while sending the request
+// The Headers can be accessed using 'req.header.Variable' object and the variables passed in the Headers
+
+app.get("/sum", (req,res)=>{
+    const val1 = parseInt(req.headers.val1)
+    const val2 = parseInt(req.headers.val2)
+
+    res.status(202).send("Sum is "+(val1+val2))
+})
+
+app.listen(port, ()=>console.log("Listening from the port: "+port))
